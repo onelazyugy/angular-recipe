9ca2acdfa147adbd8cd9b765c7ac0b51b9bfc8b3
viet <viettangle12@gmail.com>
2018-07-10 20:14:56 -0400

    wire up recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -14,9 +14,15 @@ export class RecipeListComponent implements OnInit {
     'https://www.seriouseats.com/recipes/images/2015/10/20131026-new-potatoes-roasted-crispy-thanksgiving-edit.jpg')
   ];
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(receipe: Recipe) {
+    this.recipeWasSelected.emit(receipe);
   }
 
 }
