26fe73674dd18a98e82639175af97f11d9731dca
viet <viettangle12@gmail.com>
2018-08-05 17:36:03 -0400

    added guard to route add and edit recipe if not login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,12 @@
 import * as firebase from 'firebase';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
+@Injectable()
 export class AuthService {
     token: string;
+
+    constructor(private router: Router) {}
 
     sigupUser(email: string, password: string) {
         firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(email, password)
@@ -14,6 +19,7 @@ export class AuthService {
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(
                 response => {
+                    this.router.navigate(['/']);
                     firebase.auth().currentUser.getToken()
                         .then(
                             (token: string) => this.token = token
@@ -25,11 +31,20 @@ export class AuthService {
             );
     }
 
+    logout() {
+        firebase.auth().signOut();
+        this.token = null;
+    }
+
     getToken() {
         firebase.auth().currentUser.getToken()
             .then(
                 (token: string) => this.token = token
             );
         return this.token;
+    }
+
+    isAuthenticated() {
+        return this.token != null;
     }
 }
