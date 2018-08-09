477390584802daf253ca967d9799f6e0daa84791
viet <viettangle12@gmail.com>
2018-08-08 20:48:26 -0400

    added lazy loading to recipe module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
     // an empty path is by default part of every route
     // redirect if full path is empty
-    { path: '',  redirectTo: '/recipes', pathMatch: 'full'},
+    // { path: '',  redirectTo: '/recipes', pathMatch: 'full'},
+    { path: '', component: HomeComponent },
+    // lazy loading of /recipe path. File name of the module follow by # follow by the name of the class
+    // only load the recipe module only when we visit /recipe url
+    { path: 'recipes',  loadChildren: './recipes/recipes.module#RecipesModule'},
     { path: 'shopping-list', component: ShoppingListComponent }
 ];
 @NgModule({
